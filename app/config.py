0f34c6f0e869c2a1e5067fd38bddc2b9b62e4619
oleksiy <kotov.oleksiy@icloud.com>
2026-01-26 00:01:44 +0200

    Re-write settings add token_manager.

--- app/config.py
+++ app/config.py
@@ -1,28 +1,77 @@
-from pydantic_settings import BaseSettings
-
+import os
+from pathlib import Path
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-    # DB
-    DATABASE_URL: str = "postgresql+asyncpg://admin:password@localhost:5432/movies_db"
+    # Base dir
+    BASE_DIR: Path = Path(__file__).parent.parent
+
+    # PostgreSQL
+    POSTGRES_DB: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int = 5432
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
     # JWT
-    SECRET_KEY: str = "GXrxFAYGuV84aT9Ev0jI6MqT56ZiGJWj-SyJRd618sY"
-    ALGORITHM: str = "HS256"
+    SECRET_KEY_ACCESS: str
+    SECRET_KEY_REFRESH: str
+    JWT_SIGNING_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
     # Email
-    SMTP_SERVER: str = "smtp.gmail.com"
-    SMTP_PORT: int = 587
-    SMTP_USERNAME: str = ""
-    SMTP_PASSWORD: str = ""
+    EMAIL_HOST: str
+    EMAIL_PORT: int
+    EMAIL_HOST_USER: str
+    EMAIL_HOST_PASSWORD: str
+    EMAIL_USE_TLS: bool = False
     EMAIL_FROM: str = "noreply@cinema.com"
+
+    MAILHOG_API_PORT: int = 8025
+
+    # Path to email templates
+    PATH_TO_EMAIL_TEMPLATES_DIR: str = str(BASE_DIR / "templates" / "emails")
+    ACTIVATION_EMAIL_TEMPLATE_NAME: str = "activation_request.html"
+    ACTIVATION_COMPLETE_EMAIL_TEMPLATE_NAME: str = "activation_complete.html"
+    PASSWORD_RESET_TEMPLATE_NAME: str = "password_reset_request.html"
+    PASSWORD_RESET_COMPLETE_TEMPLATE_NAME: str = "password_reset_complete.html"
+
+    # MinIO
+    S3_STORAGE_HOST: str = os.getenv("MINIO_HOST", "minio-theater")
+    S3_STORAGE_PORT: int = os.getenv("MINIO_PORT", 9000)
+    S3_STORAGE_ACCESS_KEY: str = os.getenv("MINIO_ROOT_USER", "minioadmin")
+    S3_STORAGE_SECRET_KEY: str = os.getenv("MINIO_ROOT_PASSWORD", "some_password")
+    S3_BUCKET_NAME: str = os.getenv("MINIO_STORAGE", "theater-storage")
+
+    @property
+    def S3_STORAGE_ENDPOINT(self) -> str:
+        """For MinIO."""
+        return f"http://{self.S3_STORAGE_HOST}:{self.S3_STORAGE_PORT}"
 
     # Stripe
     STRIPE_SECRET_KEY: str = ""
     STRIPE_PUBLIC_KEY: str = ""
 
     # Redis
-    REDIS_URL: str = "redis://localhost:6379"
+    REDIS_URL: str = "redis://redis:6379"
 
+    # pgAdmin
+    PGADMIN_DEFAULT_EMAIL: str = "admin@gmail.com"
+    PGADMIN_DEFAULT_PASSWORD: str = "admin"
+
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+        case_sensitive=False,
+        env_nested_delimiter="__",
+    )
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,15 +1,12 @@
 from contextlib import asynccontextmanager
-
 from fastapi import FastAPI
+from app.database import AsyncSession
 
-from app.database import engine, Base
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    yield
-    await engine.dispose()
+    async with AsyncSession() as session:
+        yield session
 app = FastAPI(
     lifespan=lifespan,
 )
